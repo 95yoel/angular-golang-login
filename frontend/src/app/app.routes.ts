b29dd6a60b3e8db4e363bb060265b224493691df
Yoel Villa <villayoel@gmail.com>
2023-11-24 11:54:17 +0100

    router and error page created

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { PrivateComponent } from './components/private/private.component';
+import { guardRouterGuard } from './guard/guard-router.guard';
+import { ErrorComponent } from './components/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    /*
+    "/" , "register" and "home"
+    goes to the same RegisterComponent
+    */
+    {path:'',component:RegisterComponent,title:'Register'},
+    {path:'register',component:RegisterComponent,title:'Register'},
+    {path:'home',redirectTo: '', pathMatch: 'full',title:'Register'},
+    {path:'login',component:LoginComponent,title:'Login'},
+    /*
+    Guard decides if the user is authorized to open private  
+    */
+    {path:'private',component:PrivateComponent,title:'Private',canActivate:[guardRouterGuard]},
+    /*
+    If the path is not in the list , go to ErrorComponent
+    */
+    {path:'**',component:ErrorComponent,title:'Error - Not Found'}
+];
--- frontend/src/app/components/error/error.component.ts
+++ frontend/src/app/components/error/error.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-error',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './error.component.html',
   styleUrl: './error.component.css'
 })
