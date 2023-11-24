b6b0fb8e6bd5dd628114b0e0c39f1bcd11c64bfa
Yoel Villa <villayoel@gmail.com>
2023-11-24 18:25:51 +0100

    functions to generate JWT and hash passowrds

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,8 @@
 module backend
 
 go 1.21.4
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	golang.org/x/crypto v0.15.0
+)
--- backend/src/auth/auth.go
+++ backend/src/auth/auth.go
@@ -1,1 +1,50 @@
 package auth
+
+import (
+	"log"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) []byte {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return hashedPassword
+}
+
+/*
+Validates the provided password against the stored hashed password.
+Returns a JWT token
+*/
+func CheckPassword(storedHashedPassword []byte, password, name, surname, email string) (string, error) {
+
+	// Validate the provided password using bcrypt
+	err := bcrypt.CompareHashAndPassword(storedHashedPassword, []byte(password))
+	if err != nil {
+		return "", err
+	}
+
+	// If the password is valid, create a new JWT token
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// Set JWT claims with user information
+	claims := token.Claims.(jwt.MapClaims)
+	claims["exp"] = time.Now().Add(time.Hour).Unix() //Set expiration time to 1 hour
+	claims["name"] = name
+	claims["surname"] = surname
+	claims["email"] = email
+
+	// Secret key to sign the token
+	tokenString, err := token.SignedString([]byte("uwu"))
+	if err != nil {
+		return "", err
+	}
+
+	// Return the generated JWT token string
+	return tokenString, nil
+}
