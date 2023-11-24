3a07ea71da2ef34cfe5c91d8b21506ba653d1d61
Yoel Villa <villayoel@gmail.com>
2023-11-24 16:42:11 +0100

    add middleware for CORS

--- backend/main.go
+++ backend/main.go
@@ -8,5 +8,6 @@ import (
 func main() {
 	fmt.Println("Backend Working !!!")
 
+	// start the server on port 8080
 	http.ListenAndServe(":8080", nil)
 }
---
+++ backend/src/database_config/main.go
@@ -0,0 +1,30 @@
+package databaseconfig
+
+import "net/http"
+
+/*
+	Middleware for handle CORS requests
+*/
+func CorsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Allow requests from all origins
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		// Allow all methods: POST, GET , PUT , DELETE
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+
+		// Set all the headers allowed
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+
+		// Handler OPTIONS requests
+		if r.Method == http.MethodOptions {
+
+			// Respond with a 200 OK status for Options Requests
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		// Pass the request to the next handler
+		next.ServeHTTP(w, r)
+	})
+}
