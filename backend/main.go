21c01a45617c04433df071ce49cad4f231bccb69
Yoel Villa <villayoel@gmail.com>
2023-11-24 16:56:32 +0100

    create the router

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	databaseconfig "backend/src/database_config"
+	"backend/src/routes"
 	"fmt"
 	"net/http"
 )
@@ -8,6 +10,12 @@ import (
 func main() {
 	fmt.Println("Backend Working !!!")
 
-	// start the server on port 8080
-	http.ListenAndServe(":8080", nil)
+	//Call the HTTP router
+	r := routes.Router()
+
+	//Apply the CORS middleware to the router
+	handler := databaseconfig.CorsMiddleware(r)
+
+	// Start the server on port 8080
+	http.ListenAndServe(":8080", handler)
 }
---
+++ backend/src/routes/routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import "net/http"
+
+/*
+Returns a router HTTP for the backend
+*/
+func Router() http.Handler {
+	r := http.NewServeMux()
+	/*
+		Write below all the routes of the backend
+	*/
+
+	return r
+}
