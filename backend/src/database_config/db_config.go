ac656201b760f347c5bdd70e84ea11e3f5471037
Yoel Villa <villayoel@gmail.com>
2023-11-25 12:48:40 +0100

    added logic to use a dev and prod environment

--- backend/src/database_config/db_config.go
+++ backend/src/database_config/db_config.go
@@ -12,26 +12,54 @@ import (
 Returns the connection string by loading configuration from db_config.json.
 */
 func GetConnectionString() string {
-	//Obtain the content in config.json
-	config, err := loadConfig("db_config.json")
-	if err != nil {
-		log.Fatal("Error loading configuration", err)
+
+	// Obtain the current enviroment
+	env := os.Getenv("ENV")
+	connectionString := ""
+
+	if env == "production" { // if the enviroment is prod , use the dbconfig_.prod file
+		config, err := loadConfigProd("db_config.prod.json")
+		if err != nil {
+			log.Fatal("Error loading configuration", err)
+		}
+		connectionString = createConnectionStringProd(config)
+	} else { // if the enviroment isn't prod , use the dbconfig_.dev file
+		config, err := loadConfigDev("db_config.dev.json")
+		if err != nil {
+			log.Fatal("Error loading configuration", err)
+		}
+		connectionString = createConnectionStringDev(config)
 	}
-	connectionString := createConnectionString(config, config.Host)
+
 	return connectionString
 }
 
 /*
-	Read the filename , parses it content to JSON , and returns a Config struct.
+Create the connectionString in the format :
+connectionString := "user=user password=password dbname=dbname  sslmode=disable"
 */
 
-func loadConfig(filename string) (models.Config, error) {
-	var config models.Config
+func createConnectionStringDev(config models.ConfigDev) string {
+	return "user=" + config.User + " password=" + config.Password + " dbname=" + config.DBname + " sslmode=disable"
+}
 
+/*
+Create the connectionString in the format :
+connectionString := "user=user password=password dbname=dbname host=host  sslmode=disable"
+*/
+
+func createConnectionStringProd(config models.ConfigProd) string {
+	return "user=" + config.User + " password=" + config.Password + " dbname=" + config.DBname + " host=" + config.Host + " sslmode=disable"
+}
+
+/*
+Load the configuration from a JSON file and unmarshal it into the specified structure.
+*/
+func loadConfig(filename string, config interface{}) error {
 	// Obtain the current directory
 	currentDir, err := os.Getwd()
 	if err != nil {
-		return config, err
+		return err
 	}
 
 	// Create the complete route
@@ -40,23 +68,27 @@ func loadConfig(filename string) (models.Config, error) {
 	// Read file from the complete route
 	file, err := os.ReadFile(filePath)
 	if err != nil {
-		return config, err
+		return err
 	}
 
-	// Unmarshal the JSON content into the Config struct
-	err = json.Unmarshal(file, &config)
-	if err != nil {
-		return config, err
-	}
-
-	//return the config
-	return config, nil
+	// Unmarshal the JSON content into the specified structure
+	return json.Unmarshal(file, config)
 }
 
 /*
-Create the connectionString in the format :
-connectionString := "user=user password=password dbname=dbname  sslmode=disable"
+Load the dev configuration and return it as a ConfigDev struct.
 */
-func createConnectionString(config models.Config, host string) string {
-	return "user=" + config.User + " password=" + config.Password + " dbname=" + config.DBname + " host=" + host + " sslmode=disable"
+func loadConfigDev(filename string) (models.ConfigDev, error) {
+	var config models.ConfigDev
+	err := loadConfig(filename, &config)
+	return config, err
+}
+
+/*
+Load the prod configuration and return it as a ConfigProd struct.
+*/
+func loadConfigProd(filename string) (models.ConfigProd, error) {
+	var config models.ConfigProd
+	err := loadConfig(filename, &config)
+	return config, err
 }
--- backend/src/models/config.go
+++ backend/src/models/config.go
@@ -1,6 +1,18 @@
 package models
 
 type Config struct {
+	User     string `json:"user"`
+	DBname   string `json:"dbname"`
+	Password string `json:"password"`
+}
+
+type ConfigDev struct {
+	User     string `json:"user"`
+	DBname   string `json:"dbname"`
+	Password string `json:"password"`
+}
+
+type ConfigProd struct {
 	Host     string `json:"host"`
 	User     string `json:"user"`
 	DBname   string `json:"dbname"`
